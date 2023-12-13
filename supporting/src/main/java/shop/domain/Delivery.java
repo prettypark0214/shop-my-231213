8ca53ecf27e811066dff5fb48f9a41ec6f55f928
prettypark0214 <153568027+prettypark0214@users.noreply.github.com>
2023-12-13 11:38:21 +0900

    commit

---
+++ supporting/src/main/java/shop/domain/Delivery.java
@@ -0,0 +1,107 @@
+package shop.domain;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+import shop.SupportingApplication;
+import shop.domain.DeliveryCanceled;
+import shop.domain.DeliveryStarted;
+
+@Entity
+@Table(name = "Delivery_table")
+@Data
+//<<< DDD / Aggregate Root
+public class Delivery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String orderid;
+
+    private String customerid;
+
+    private String productid;
+
+    private Integer qty;
+
+    private String status;
+
+    private String address;
+
+    @PostPersist
+    public void onPostPersist() {
+        DeliveryStarted deliveryStarted = new DeliveryStarted(this);
+        deliveryStarted.publishAfterCommit();
+
+        DeliveryCanceled deliveryCanceled = new DeliveryCanceled(this);
+        deliveryCanceled.publishAfterCommit();
+    }
+
+    public static DeliveryRepository repository() {
+        DeliveryRepository deliveryRepository = SupportingApplication.applicationContext.getBean(
+            DeliveryRepository.class
+        );
+        return deliveryRepository;
+    }
+
+    //<<< Clean Arch / Port Method
+    public static void startDelivery(OrderPlaced orderPlaced) {
+        //implement business logic here:
+
+        /** Example 1:  new item 
+        Delivery delivery = new Delivery();
+        repository().save(delivery);
+
+        DeliveryStarted deliveryStarted = new DeliveryStarted(delivery);
+        deliveryStarted.publishAfterCommit();
+        */
+
+        /** Example 2:  finding and process
+        
+        repository().findById(orderPlaced.get???()).ifPresent(delivery->{
+            
+            delivery // do something
+            repository().save(delivery);
+
+            DeliveryStarted deliveryStarted = new DeliveryStarted(delivery);
+            deliveryStarted.publishAfterCommit();
+
+         });
+        */
+
+    }
+
+    //>>> Clean Arch / Port Method
+    //<<< Clean Arch / Port Method
+    public static void cancelDelivery(OrderCanceled orderCanceled) {
+        //implement business logic here:
+
+        /** Example 1:  new item 
+        Delivery delivery = new Delivery();
+        repository().save(delivery);
+
+        DeliveryCanceled deliveryCanceled = new DeliveryCanceled(delivery);
+        deliveryCanceled.publishAfterCommit();
+        */
+
+        /** Example 2:  finding and process
+        
+        repository().findById(orderCanceled.get???()).ifPresent(delivery->{
+            
+            delivery // do something
+            repository().save(delivery);
+
+            DeliveryCanceled deliveryCanceled = new DeliveryCanceled(delivery);
+            deliveryCanceled.publishAfterCommit();
+
+         });
+        */
+
+    }
+    //>>> Clean Arch / Port Method
+
+}
+//>>> DDD / Aggregate Root
---
+++ supporting/src/main/java/shop/domain/DeliveryStarted.java
@@ -0,0 +1,29 @@
+package shop.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import lombok.*;
+import shop.domain.*;
+import shop.infra.AbstractEvent;
+
+//<<< DDD / Domain Event
+@Data
+@ToString
+public class DeliveryStarted extends AbstractEvent {
+
+    private Long id;
+    private String orderid;
+    private String customerid;
+    private String productid;
+    private Integer qty;
+    private String address;
+
+    public DeliveryStarted(Delivery aggregate) {
+        super(aggregate);
+    }
+
+    public DeliveryStarted() {
+        super();
+    }
+}
+//>>> DDD / Domain Event
